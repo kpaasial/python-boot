a008aeefbdd29fc5d5bc159466396f1f33aa7e46
Kimmo Paasiala <kpaasial@gmail.com>
2012-07-22 04:52:31 +0300

    Added one TODO comment.

--- utils/pb_utils.py
+++ utils/pb_utils.py
@@ -40,6 +40,7 @@ def get_make_variable(variable, directory=''):
 # Returns a list of installed packages, for each package
 # the origin (port directory relative to PORTSDIR) of the
 # package is returned.
+# TODO Add options for origin etc.
 def get_installed_packages():
 	cmdLine = [PKGNG]
 	cmdLine.append('info')
